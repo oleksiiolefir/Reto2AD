58050fb175100f1d428b764578ff8d9571cb0a87
IN1DM3B_06 <IN1DM3B_06@H015112.NET>
2019-10-22 08:08:30 +0200

    subida de la clase de lector CSV

--- Reto2AD/src/main/java/Launcher/launcher.java
+++ Reto2AD/src/main/java/Launcher/launcher.java
@@ -1,12 +1,19 @@
 package Launcher;
 
+import java.io.IOException;
+
 import modelo.Conexion;
+import modelo.lecturaCSV;
 
 public class launcher {
-
-	public static void main(String[] args) {
+	static String archivoCsv = "ArchivoCSV.csv";
+	
+	public static void main(String[] args) throws IOException {
 		Conexion c = new Conexion();
 		c.conectar();
+		
+		
+		lecturaCSV.funcionesDeLectorCsv(archivoCsv);
 	}
 
 }
